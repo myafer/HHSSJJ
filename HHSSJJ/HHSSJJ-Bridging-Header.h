294ecf0ab4526138ec0a756a7262308bdc2b349b
afer <koudaiwang@koudaiwangdeMac-mini.local>
2016-11-30 16:07:03 +0800

    init
    init

---
+++ HHSSJJ/HHSSJJ-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  HHSSJJ-Bridging-Header.h
+//  HHSSJJ
+//
+//  Created by 口贷网 on 16/11/30.
+//  Copyright © 2016年 Afer. All rights reserved.
+//
+
+#ifndef HHSSJJ_Bridging_Header_h
+#define HHSSJJ_Bridging_Header_h
+
+#import "AFNetworking.h"
+
+#endif /* HHSSJJ_Bridging_Header_h */
---
+++ HHSSJJ/main.swift
@@ -0,0 +1,46 @@
+//
+//  main.swift
+//  HHSSJJ
+//
+//  Copyright © 2016年 Afer. All rights reserved.
+//
+
+import Foundation
+
+
+
+if CommandLine.arguments.count != 4 {
+    print("输入有误！")
+    print("输入顺序为 ukey _api_key path")
+    exit(0)
+}
+
+var path = ""
+if CommandLine.arguments.count > 1 {
+    path = CommandLine.arguments[3]
+}
+
+
+
+let manager = AFHTTPSessionManager()
+manager.responseSerializer.acceptableContentTypes = NSSet.init(array: ["text/html","text/json","application/json","text/javascript"]) as? Set<String>
+manager.requestSerializer = AFJSONRequestSerializer()
+manager.post("http://www.pgyer.com/apiv1/app/upload", parameters: ["uKey":CommandLine.arguments[1], "_api_key":CommandLine.arguments[2]], constructingBodyWith: { (formData) in
+        let dd:AFMultipartFormData = (formData as AFMultipartFormData)
+        var data: NSData? = NSData.init(contentsOfFile: path)
+        dd.appendPart(withFileData: data as! Data, name: "file", fileName: "123.ipa", mimeType: "")
+    }, progress: { (pro) in
+         print((pro as Progress).fractionCompleted)
+        
+    }, success: { (task, obj) in
+        print(obj)
+        print("成功")
+        exit(0)
+    }) { (task, error) in
+        print("失败", error)
+         exit(0)
+    }
+
+RunLoop.current.run()
+
+
